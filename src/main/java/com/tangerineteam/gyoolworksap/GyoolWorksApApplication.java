1344967fcfe0f7292851a9be25921c4fb4c3abe7
junsu <zzansu01@gmail.com>
2025-05-26 09:38:02 +0900

    first commit

---
+++ src/main/java/com/tangerineteam/gyoolworksap/GyoolWorksApApplication.java
@@ -0,0 +1,13 @@
+package com.tangerineteam.gyoolworksap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyoolWorksApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyoolWorksApApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tangerineteam/gyoolworksap/GyoolWorksApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangerineteam.gyoolworksap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyoolWorksApApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
